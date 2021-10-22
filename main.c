3bd44e11ba8b2a3e1942d2667fb01df33941f3d4
IreneKibui <92978177+IreneKibui@users.noreply.github.com>
2021-10-22 20:41:14 +0300

    Update main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+    Project: Library System
+    Author: Irine Kibui
+    Date: 22-10-2021
+    Compiler: GCC
+    Licence: MIT
+*/
+
+#include<stdio.h>
+
+int main(){
+    printf("\tWelcome to Kiambu Library\n");
+    printf("Miss Irine\n");
+    printf("1:Add user\n");
+    printf("2:Add Book\n");
+    printf("3:Borrow Book\n");
+    printf("4:Return Book\n");
+    printf("5:Delete User\n");
+    printf("6:Delete book\n");
+    printf("7:Logout\n");
+
+    return 0;
+}
